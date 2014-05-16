8c7a0199ff4b9a47abfab58b8ede6f9519c32b3b
nazoking <nazoking@gmail.com>
2014-05-16 21:07:04 +0900

    add coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'rubygems'
 require 'rehtml'
+require 'coveralls'
+Coveralls.wear!
 
 RSpec::Matchers.define :my_matcher do |expected|
   match do |actual|
