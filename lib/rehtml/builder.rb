61f7989891580e28278374784d16064ffabda8dd
nazoking <nazoking@gmail.com>
2014-05-23 22:02:14 +0900

    format and add comment

--- lib/rehtml/builder.rb
+++ lib/rehtml/builder.rb
@@ -3,15 +3,54 @@ module REHTML
     EMPTY_TAGS=Set.new %w[br img hr meta input embed area base col keygen link param source isindex basefont]
     CDATA_TAGS=Set.new %w[script style textarea xmp title]
     attr_reader :doc
-    def initialize
+
+    # build document use tokenizer
+    def parse(tokenizer)
       @doc = REXML::Document.new
       @pos = @doc
-    end
-    def parse(tokenizer)
       while node=tokenizer.next
         append(node)
       end
     end
+
+    # append node to document
+    def append(node)
+      if node.is_a?(EndTag)
+        return if empty_tag?(node.name)
+        po = @pos
+        while po.parent and po.name != node.name
+          po = po.parent
+        end
+        if po.name == node.name
+          @pos = po.parent
+        end
+      else
+        rexml = to_rexml(node)
+
+        # if node is second root element, add root element wrap html tag
+        if rexml.is_a?(REXML::Element) and @pos == @doc and @doc.root
+          if @doc.root.name != 'html'
+            html = REXML::Element.new
+            html.name = "html"
+            i = @doc.root.index_in_parent-1
+            while pos = @doc.delete_at(i)
+              @doc.delete_element(pos) if pos.is_a?(REXML::Element)
+              html << pos
+            end
+            @doc << html
+            @pos = html
+          end
+          @pos = @doc.root
+        end
+        @pos << rexml
+        if rexml.is_a?(REXML::Element) and !empty_tag?(node.name) and !node.empty?
+          @pos = rexml
+        end
+      end
+    end
+
+    private
+
     def to_rexml(node)
       case node
       when Text
@@ -43,43 +82,11 @@ module REHTML
         raise "unknown node type #{node}"
       end
     end
-    def append(node)
-      if node.is_a?(EndTag)
-        return if empty_tag?(node.name)
-        po = @pos
-        while po.parent and po.name != node.name
-          po = po.parent
-        end
-        if po.name == node.name
-          @pos = po.parent
-        end
-      else
-        rexml = to_rexml(node)
 
-        # if second root
-        if rexml.is_a?(REXML::Element) and @pos == @doc and @doc.root
-          if @doc.root.name != 'html'
-            html = REXML::Element.new
-            html.name = "html"
-            i = @doc.root.index_in_parent-1
-            while pos = @doc.delete_at(i)
-              @doc.delete_element(pos) if pos.is_a?(REXML::Element)
-              html << pos
-            end
-            @doc << html
-            @pos = html
-          end
-          @pos = @doc.root
-        end
-        @pos << rexml
-        if rexml.is_a?(REXML::Element) and !empty_tag?(node.name) and !node.empty?
-          @pos = rexml
-        end
-      end
-    end
     def empty_tag?(tagname)
       EMPTY_TAGS.include?(tagname)
     end
+
     def cdata_tag?(tagname)
       CDATA_TAGS.include?(tagname)
     end
