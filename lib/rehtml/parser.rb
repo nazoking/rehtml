0d3d84c887b299412d5dc18aa2d9da9869e1fb5a
nazoking <nazoking@gmail.com>
2014-05-23 21:54:04 +0900

    remove unused method

--- lib/rehtml/parser.rb
+++ lib/rehtml/parser.rb
@@ -7,16 +7,4 @@ module REHTML
     builder.parse(Tokenizer.new(str))
     builder.doc
   end
-
-  private
-
-  def self.node_to_remlx(html)
-    ret = html.to_rexml
-    if html.is_a?(Parent)
-      for node in html.children
-        ret << node_to_remlx(node)
-      end
-    end
-    ret
-  end
 end
